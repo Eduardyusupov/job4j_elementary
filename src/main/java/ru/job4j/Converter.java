ab046f92d9fca021bdcc72e6c8c6a2312630b55d
EYusupov <usupov-1992@yandex.ru>
2025-04-14 08:25:36 +0300

    Модульные тесты [#505277]

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -1,22 +1,11 @@
 package ru.job4j;
 
 public class Converter {
-    /**
-     * Метод проверяет конвертацию рублей к евро.
-     *
-     * @param value Количество рублей для конвертации
-     * @return Конвертиованные в евро рубли.
-     */
+
     public static float rubleToEuro(float value) {
         return value / 90;
     }
 
-    /**
-     * Метод проверяет конвертацию рублей к доллару.
-     *
-     * @param value Количество рублей для конвертации
-     * @return Конвертиованные в доллары рубли.
-     */
     public static float rubleToDollar(float value) {
         return value / 80;
     }
