12fd8a701ce082976212ce5a796c4a14a670e2d4
EYusupov <usupov-1992@yandex.ru>
2025-04-21 08:48:12 +0300

    Сумма чисел [#505208 #540653]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,8 +3,8 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (int startNumber = start; startNumber <= finish; startNumber++) {
-            sum = sum + startNumber;
+        for (int i = start; i <= finish; i++) {
+            sum = sum + i;
         }
         return sum;
     }
