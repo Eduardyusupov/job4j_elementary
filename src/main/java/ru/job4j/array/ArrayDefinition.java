edab94cc0db1dafa3f14462b6546e50d0e4c2960
EYusupov <usupov-1992@yandex.ru>
2025-05-02 07:00:30 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,6 +6,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Ivan";
+        names[1] = "Vladimir";
+        names[2] = "Igor";
+        names[3] = "Vladislav";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
 
         System.out.println("Размер short массива = " + ages.length);
         System.out.println("Размер String массива = " + surnames.length);
